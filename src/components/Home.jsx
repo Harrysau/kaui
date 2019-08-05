547b3c0d61d6796dc7e03eecf4a29fe938813ccd
Harry <harrysauchhinsun@ymail.com>
2019-08-05 23:33:17 +0700

    change nav style and homepage and add 1 new package

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <h1 className="text-2xl">Home Page</h1>
-        </div>
-    );
+  return (
+    <div className="container mx-auto shadow-xl border-solid border-black rounded-lg h-screen mt-20 w-full">
+      <div className="h-full z-10 text-center">
+        <h1 className="text-2xl z-10 justify-center">Home Page</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
