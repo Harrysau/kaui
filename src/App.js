6060dfb250546117d7c97c2103f76ec73fadd2ba
Harrysau <harrysauchhinsun@ymail.com>
2019-08-06 20:49:24 +0700

    minor

--- src/App.js
+++ src/App.js
@@ -2,28 +2,20 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
-import { Helmet } from "react-helmet";
 import "./App.css";
 import "./styles/index.css";
 
 function App() {
-  const styles = {};
-  return (
-    <div>
-      <Helmet
-        bodyAttributes={{
-          style:
-            "background-image : linear-gradient(to bottom, #9fa8a3 70%, #e3e0cf)"
-        }}
-      />
-      <NavBar />
-      <Router>
-        <Switch>
-          <Route path="/" component={Home} />
-        </Switch>
-      </Router>
-    </div>
-  );
+    return (
+        <div>
+            <NavBar />
+            <Router>
+                <Switch>
+                    <Route path="/" component={Home} />
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+import "../assets/css/home.css";
 
 const Home = () => {
-  return (
-    <div className="container mx-auto shadow-xl border-solid border-black rounded-lg h-screen mt-20 w-full">
-      <div className="h-full z-10 text-center">
-        <h1 className="text-2xl z-10 justify-center">Home Page</h1>
-      </div>
-    </div>
-  );
+    return (
+        <div className="m-0 p-0">
+            <Helmet
+                bodyAttributes={{
+                    style:
+                        "background-image : linear-gradient(to top right, #C6FFDD, #FBD786, #f7797d)"
+                }}
+            />
+            <div className="container mx-auto shadow-xl border-solid border-black rounded-lg home-height mt-20 w-3/4">
+                <div className="absolute bg-gray-700 w-3/4 opacity-25 home-height rounded-lg" />
+                <div className="absolute  w-3/4 rounded-lg text-center">
+                    Homepage
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
